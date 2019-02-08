7f415542051ce1c1e03f9b6f81374580f9940fc5
rosscwagner <rosscwagner@gmail.com>
2019-02-08 13:26:40 -0500

    Gauss filter working

---
+++ README.md
@@ -0,0 +1,3 @@
+This is best viewed with a markdown renderer to view embedded images: https://atom.io/packages/markdown-preview
+
+ 
--- src/pa1.py
+++ src/pa1.py
@@ -12,12 +12,16 @@ to the image and displays the result. resulting image saved in ../outputImages/m
 from PIL import Image
 import sys
 import os
-import array
 import statistics
+import numpy as np
+from scipy.stats import multivariate_normal
+import math
+import cv2
 
 
 def main():
-    if sys.argv.__len__() > 4 or sys.argv.__len__() < 2:
+    numArgs = sys.argv.__len__()
+    if numArgs > 5 or numArgs < 2:
         print("Wrong number of command line arguments")
         print("For help: python3 pa1.py -help")
         exit(1)
@@ -40,15 +44,24 @@ def main():
         filteredBands = []
         newImage = None
 
-        size = int(sys.argv[3])
-        # check for ../outputImages directory
-        if not os.path.isdir("../outputImages"):
-            os.makedirs("../outputImages")
+        # get size input and make sure it is valid
+        try:
+            size = int(sys.argv[3])
+        except ValueError:
+            print("Size input must be a base 10 integer")
+            print("For help: python3 pa1.py -help")
+            exit(1)
 
         if size % 2 != 1:
             print("Filter size must be odd (3,5,7,...ect)")
             print("For help: python3 pa1.py -help")
             exit(1)
+
+        # check for ../outputImages directory
+        if not os.path.isdir("../outputImages"):
+            os.makedirs("../outputImages")
+
+        # apply actions
         if action == "b":
             print("Applying box filter size {0} on {1}".format(size, filePath))
             for band in bands:
@@ -57,11 +70,24 @@ def main():
             newImage.save("../outputImages/boxFilter{0}{1}".format(size, file), image.format)
 
         elif action == "g":
-            print("Apply Gaussian size {0} on {1}".format(size, filePath))
+            # get sigma
+            if numArgs == 5:
+                try:
+                    sigma = float(sys.argv[4])
+                except ValueError:
+                    print("Sigma input must be a base 10 float")
+                    print("For help: python3 pa1.py -help")
+                    exit(1)
+            else:
+                print("Sigma argument needed")
+                print("For help: python3 pa1.py -help")
+                exit(1)
+
+            print("Applying Gaussian filter size {0} with sigma {1} on {2}".format(size, sigma, filePath))
             for band in bands:
-                filteredBands.append(gaussianFilter(band, size))
+                filteredBands.append(gaussianFilter2D(band, size, sigma))
             newImage = Image.merge(mode, filteredBands)
-            # newImage.save("../outputImages/gaussianFilter{0}{1}".format(size, file), image.format)
+            newImage.save("../outputImages/gaussianFilter{0}-{1}{2}".format(size, sigma, file), image.format)
         elif action == "m":
             print("Applying median filter size {0} on {1}".format(size, filePath))
             for band in bands:
@@ -117,9 +143,61 @@ def boxFilter(image,size):
             newPixels[i, j] = tempPix
     return newImg
 
+def gaussian(sigma, value):
+    return (1/(math.sqrt(2*math.pi)*sigma))*math.exp(-(value*value)/(2*sigma*sigma))
 
-def gaussianFilter(image, size):
-    return image
+
+'''
+ToDo
+'''
+def gaussianFilter2D(image, size, sigma):
+    # iterate through each pixel
+    delta = size // 2  # distance from center pixel to edge of filter
+
+    a = np.linspace(0, size, size, endpoint=False)
+    #b = multivariate_normal.pdf(a, mean=0, cov=sigma)
+    for val in range(size):
+        a[val] = gaussian(sigma, val-delta)
+    b = a.reshape(1, size)
+
+    gaussFilter = np.dot(b.T, b)
+    #print(gaussFilter)
+
+    # get the sum of the filter
+    normalizer = sum2DMatrix(gaussFilter)
+
+
+    pixelMap = image.load()
+    newImg = Image.new(image.mode, image.size)
+    newPixels = newImg.load()
+
+
+    #newImg = cv2.filter2D(image,-1,gaussFilter)
+
+    for x in range(newImg.size[0]):
+        for y in range(newImg.size[1]):
+            # apply filter
+            tempPix = 0.0
+            for filterX in range(size):
+
+                for filterY in range(size):
+                    # make sure filter is in range
+                    targetX = x-delta+filterX
+                    targetY = y-delta+filterY
+                    if not (targetX < 0 or targetX >= newImg.size[0] or targetY < 0 or targetY >= newImg.size[1]):
+
+                        try:
+                            tempPix += float(gaussFilter[filterX][filterY]) * float(pixelMap[targetX, targetY])
+                        except IndexError:
+                            print("k: {0} |  l: {1}".format(filterX, filterY))
+                            print("{0} | {1}".format(newImg.size[0], newImg.size[1]))
+                            exit(1)
+
+
+
+            newPixels[x, y] = int(tempPix/normalizer)
+
+    return newImg
 
 
 def medianFilter(image, size):
@@ -159,19 +237,28 @@ def medianFilter(image, size):
     return newImg
 
 def printHelp():
-    print("Usage: python3 pa1.py -<action to take> <path to image>  [<size of filter. An odd number>]")
+    print("Usage: python3 pa1.py -<action> <path to image> [<size of filter. An odd number>] [<sigma for Gauss>]")
     print("Example: python3 pa1.py -m ../inputImages/image1.png  5")
     print("Apply image processing algorithms to apply filters, generate histograms, or apply Canny edge detection.\n")
     print("Potential mandatory arguments:")
     print("\t-b\t apply box filter. Size input needed.")
     print("\t-c\t apply Canny edge detection.")
 
-    print("\t-g\t apply Gaussian filter. Size input needed.")
+    print("\t-g\t apply Gaussian filter. Size input needed. Sigma input needed")
     print("\t-h\t generate a histogram of the image's color frequency.")
 
     print("\t-help\t print help.")
 
     print("\t-m\t apply median filter. Size input needed.")
+
+
+def sum2DMatrix(matrix):
+    sum = 0.0
+    for row in matrix:
+        for elem in row:
+            sum += elem
+    return sum
+
 
 
 
